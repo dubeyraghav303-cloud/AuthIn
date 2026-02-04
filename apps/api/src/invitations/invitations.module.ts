795cc4053f52e2786ef2ed4c4b8d5d726723eca5
Raghav Dubey <raghavdubey@Raghavs-MacBook-Air.local>
2026-02-04 13:48:07 +0530

    fix: import UsersModule in InvitationsModule to resolve TenantGuard dependency

--- apps/api/src/invitations/invitations.module.ts
+++ apps/api/src/invitations/invitations.module.ts
@@ -3,7 +3,10 @@ import { InvitationsService } from './invitations.service';
 import { InvitationsController } from './invitations.controller';
 import { PrismaService } from '../prisma/prisma.service';
 
+import { UsersModule } from '../users/users.module';
+
 @Module({
+    imports: [UsersModule],
     controllers: [InvitationsController],
     providers: [InvitationsService, PrismaService],
     exports: [InvitationsService],
